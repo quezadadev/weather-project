0a4d70a25b6107d682de09b387b3b96bb03989e8
jesusquezada <[]>
2022-06-02 13:40:52 -0700

    style(CardsContainer): updated Cards layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import Home from "./pages/Home";
 
 const App = () => {
@@ -7,13 +6,6 @@ const App = () => {
   return (
     <div>
      <Home />
-=======
-
-const App = () => {
-  return (
-    <div>
-     <h1>App</h1>
->>>>>>> dd972cf8781d856a971330b75b598cd6db03b0f2
     </div>
   );
 }
