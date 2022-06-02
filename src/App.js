4004deb62d1d36e2aeb0518b6249ada8606c094e
jesusquezada <[]>
2022-06-02 13:57:06 -0700

    feat(Header): created Header Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 
 const App = () => {
   
   return (
     <div>
-     <Home />
+    <Header />
+    <Home />
     </div>
   );
 }
