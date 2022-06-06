3e636273e909e08b4e3883473c22e9b3757e4098
jesusquezada <[]>
2022-06-06 15:26:55 -0700

    bug(Home): Seeing git error on landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,25 +4,41 @@ import axios from "axios";
 
 const Home = () => {
 
-    const [data, setData] = useState({});
+    // const [data, setData] = useState({});
+    const [weather, setWeather] = useState({});
   
-    const getData = async () => {
-    try { 
-      const URL = `https://ipgeolocation.abstractapi.com/v1/?api_key=${process.env.REACT_APP_ABSTRACT_KEY}`;
-      const res  = await axios(URL);
-      setData(res.data);
-    } 
-    catch (err) {
-    throw new Error('Unable to get a token.')
-  }
-    };
+  //   const getData = async () => {
+  //   try { 
+  //     const URL = `https://api.geoapify.com/v1/geocode/search?city="San Diego"&apiKey=${process.env.REACT_APP_GEOAPIFY_KEY}`;
+  //     const res  = await axios(URL);
+  //     setData(res.data);
+  //   } 
+  //   catch (err) {
+  //   throw new Error('Unable to get a token.')
+  // }
+  //   };
+
+    const getWeather = async () => {
+      try { 
+        const URL = `https://api.weatherbit.io/v2.0/forecast/daily?city=Berlin&key=${process.env.REACT_APP_WEATHERBIT_KEY}`;
+        const res  = await axios(URL);
+        setWeather(res.data);
+      } 
+      catch (err) {
+      throw new Error('Unable to get a token.')
+    }
+      };
   
     useEffect(() => {
-        getData();
+        // getData();
+        getWeather();
     }, []);
 
   // console.log(`https://ipgeolocation.abstractapi.com/v1/?api_key=${process.env.REACT_APP_ABSTRACT_KEY}&postal_code=94103`);
   // console.log(data);
+  console.log(weather)
+  // console.log(`https://api.weatherbit.io/v2.0/forecast/daily?city=Frankfurt&key=${process.env.REACT_APP_WEATHER_BIT_KEY}`);
+  console.log(process.env.REACT_APP_WEATHERBIT_KEY);
 
   return (
     <>
