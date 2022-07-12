a460cd30a3bb558c271a4fd9844de62bd24d0274
jesusquezada <[]>
2022-07-12 01:53:40 -0700

    update(Header): updated navbar text

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,52 +10,11 @@ const Header = () => {
       light
     >
       <NavbarBrand href="/">
-        reactstrap
+        <h2 className='text-center'> Weatherboard </h2>
       </NavbarBrand>
       <NavbarToggler onClick={function noRefCheck(){}} />
-      <Collapse navbar>
-        <Nav
-          className="me-auto"
-          navbar
-        >
-          <NavItem>
-            <NavLink href="/components/">
-              Components
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="https://github.com/reactstrap/reactstrap">
-              GitHub
-            </NavLink>
-          </NavItem>
-          <UncontrolledDropdown
-            inNavbar
-            nav
-          >
-            <DropdownToggle
-              caret
-              nav
-            >
-              Options
-            </DropdownToggle>
-            <DropdownMenu right>
-              <DropdownItem>
-                Option 1
-              </DropdownItem>
-              <DropdownItem>
-                Option 2
-              </DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem>
-                Reset
-              </DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-        </Nav>
-        <NavbarText>
-          Simple Text
-        </NavbarText>
-      </Collapse>
+      {/* <h2 className='text-center'> Weatherboard </h2> */}
+      
     </Navbar>
   </div>
   )
